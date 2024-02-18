7635c13fcd30a0e53fcdbd2eb4d0800a624ed673
Heimatseiten <159701446+Heimatseiten@users.noreply.github.com>
2024-02-18 20:46:38 +0100

    injected more weatherdata

--- src/index.js
+++ src/index.js
@@ -2,8 +2,37 @@ function refreshWeather(respond) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = respond.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(respond.data.time * 1000);
   cityElement.innerHTML = respond.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = respond.data.condition.description;
+  humidityElement.innerHTML = `${respond.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${respond.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -11,14 +40,11 @@ function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
 searchCity("Cologne");
